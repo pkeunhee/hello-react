a2e35462612b6441fa2e22bd0ebb33b528a0546c
ghp-mac <pakkeunhee@gmail.com>
2018-05-02 23:47:19 +0900

    수정 기능 추가

--- src/App.js
+++ src/App.js
@@ -24,15 +24,36 @@ class App extends Component {
       this.setState({
           information: information.concat({ id: this.id++, ...data})
       })
-  }  
+  }
+  
+  handleRemove = (id) => {
+      const { information } = this.state;
+      this.setState({
+          information: information.filter(info => info.id !== id)
+      });
+  }
+  
+  handleUpdate = (id, data) => {
+      const { information } = this.state;
+      this.setState({
+          information: information.map(
+                  info => id === info.id ? { ...info, ...data } : info
+          )
+      })
+  }
     
   render() {
+    const { information } = this.state;
     return (
       <div>
           <PhoneForm 
               onCreate={this.handleCreate}
           />
-          <PhoneInfoList data={this.state.information} />
+          <PhoneInfoList 
+              data={this.state.information}
+              onRemove={this.handleRemove}
+              onUpdate={this.handleUpdate}
+          />
        </div>
     );
   }
--- src/PhoneInfo.js
+++ src/PhoneInfo.js
@@ -10,6 +10,47 @@ class PhoneInfo extends Component {
         }
     }
     
+    state = {
+        editing: false,
+        name: '',
+        phone: ''
+    }
+    
+    handleRemove = () => {
+        const { info, onRemove } = this.props;
+        onRemove(info.id);
+    }
+    
+    handleToggleEdit = () => {
+       const { editing } = this.state;
+       this.setState({ editing: !editing });
+    }
+    
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+        const { info, onUpdate } = this.props;
+        if(!prevState.editing && this.state.editing){
+            this.setState({
+                name: info.name,
+                phone: info.phone
+            })
+        }
+        
+        if(prevState.editing && !this.state.editing){
+            onUpdate(info.id, { 
+                name: this.state.name,
+                phone: this.state.phone
+            });
+        }
+    }
+    
+    
     render() {
         const style = {
                 border: '1px solid black',
@@ -17,14 +58,45 @@ class PhoneInfo extends Component {
                 margin: '8px'
         };
         
+        const { editing } = this.state;
+        
+        //수정모드
+        
+        if(editing){
+            return (
+                    <div style={style}>
+                        <div>
+                            <input
+                                value={this.state.name}
+                                name="name"
+                                placeholder="이름"
+                                onChange={this.handleChange}
+                            />" +
+                        </div>
+                        <div>
+                            <input
+                                value={this.state.phone}
+                                name="phone"
+                                placeholder="전화번호"
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <button onClick={this.handleToggleEdit}>적용</button>
+                        <button onClick={this.handleRemove}>삭제</button>
+                    </div>
+            );
+        }
+        
         const {
-            name, phone, id
+            name, phone
         } = this.props.info;
         
         return (
                 <div style={style}>
                     <div><b>{name}</b></div>
                     <div>{phone}</div>
+                    <button onClick={this.handleToggleEdit}>수정</button>
+                    <button onClick={this.handleRemove}>삭제</button>
                 </div>
         );
     }
